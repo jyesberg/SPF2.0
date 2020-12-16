5c273813914f41a19624083ab8389f40f8ba821c
jyesberg <joshua.yesberg@act.gov.au>
2020-12-16 15:40:52 +1100

    Update nav.js
    Updated for new nav

--- js/nav.js
+++ js/nav.js
@@ -1,35 +1,66 @@
-//Navigation toggle
-var navActive = false,
-    navOpen = document.querySelector('.nav-toggle-menu'),
-    navListOpen = document.querySelector('.c-primary-nav');
-
-navOpen.addEventListener("click", function(event){
-	event.preventDefault();
-
-    if (navActive === false) {
-	    navActive = true;
-	    navListOpen.classList.add("c-primary-nav--is-active");
-	} 
-	else {
-		 navActive = false;
-		 navListOpen.classList.remove("c-primary-nav--is-active");
-	}
+// if scroll top > 163
+$(window).scroll(function() {
+    163 < $(window).scrollTop() ? $('nav.top-navigation').addClass('top') : $('nav.top-navigation').removeClass('top');
 });
 
-//Search form toggle
-var searchActive = false,
-    searchOpen = document.querySelector('.nav-toggle-search'),
-    searchFormOpen = document.querySelector('.c-search-form');
-    
-    searchOpen.addEventListener("click", function(event){
-	event.preventDefault();
 
-    if (searchActive === false) {
-	    searchActive = true;
-	    searchFormOpen.classList.add("c-search-form--is-active");
-	} 
-	else {
-		 searchActive = false;
-		 searchFormOpen.classList.remove("c-search-form--is-active");
-	}
+//mobile nav toggle
+$(".nav-btn").click(function () {
+$(this).toggleClass("is-active"),
+    $(".cover").toggleClass("show"),
+    $(".nav-btn.is-active").length
+        ? ($(this).attr("aria-expanded", "true"), $(".cover").addClass("show"), $("body").addClass("stop-scrolling"), $(".search-btn").prop("disabled", !0))
+        : ($(this).attr("aria-expanded", "false"), $(".cover").removeClass("show"), $("body").removeClass("stop-scrolling"), $(".search-btn").prop("disabled", !1)),
+    $("nav.top-navigation").toggleClass("is-active"),
+    setTimeout(function () {
+        $("nav .nav-container .btn--close").focus();
+    }, 100);
+}),
+
+$(".cover").click(function () {
+    $("nav.top-navigation").hasClass("is-active") &&
+        ($(this).removeClass("show"),
+        $(".nav-btn").removeClass("is-active"),
+        $("nav.top-navigation").removeClass("is-active"),
+        $(".nav-btn").attr("aria-expanded", "false"),
+        $("body").removeClass("stop-scrolling"),
+        $(".search-btn").prop("disabled", !1)),
+        $(".nav-search-container.show").length &&
+            ($(".cover").removeClass("show"),
+            $("body").removeClass("stop-scrolling"),
+            $(".header-container .search-container a").attr("tabindex", "0"),
+            $(".header-container .search-container input").attr("tabindex", "0"),
+            $(".header-container .search-container button").attr("tabindex", "0"),
+            $(".nav-btn").attr("tabindex", "0"),
+            $(".nav-btn").prop("disabled", !1),
+            $(".search-btn").removeClass("open"),
+            $(".search-btn i").addClass("fa-search"),
+            $(".search-btn i").removeClass("fa-close"),
+            $(".nav-search-container").removeClass("show"));
+}),
+
+$(".btn--close").click(function () {
+    $(".nav-btn").removeClass("is-active"),
+        $(".cover").removeClass("show"),
+        $("nav.top-navigation").removeClass("is-active"),
+        $(".nav-btn").attr("aria-expanded", "false"),
+        $(".nav-btn").focus(),
+        $("body").removeClass("stop-scrolling"),
+        $(".search-btn").prop("disabled", !1);
+}),
+$("body").on("keydown", "nav .nav-container .btn--close", function (e) {
+    $("nav.is-active").length && e.shiftKey && 9 == e.which && (e.preventDefault(), $("nav .nav-container a:last").focus());
+}),
+$("body").on("keydown", "nav .nav-container a:last", function (e) {
+    $("nav.is-active").length && (e.shiftKey || (9 == e.which && (e.preventDefault(), $("nav .nav-container .btn--close").focus())));
+});
+
+
+//Expand the sub navigation on the mobile menu
+$(".expand").click(function (e) {
+    e.preventDefault();
+    $(this).toggleClass("active");
+    //adding twice due to conflict
+    $(this).toggleClass("active");
+    $(this).parent().siblings("ul").toggleClass("show");
 });
